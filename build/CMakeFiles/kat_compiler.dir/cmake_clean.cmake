7ff9bdb8108324b8899e4a54c1c75fe9b81a2185
Abhishek Anand <acedmicabhishek@gmail.com>
2025-01-04 13:32:42 +0530

    added parser

---
+++ build/CMakeFiles/kat_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kat_compiler.dir/link.d"
+  "CMakeFiles/kat_compiler.dir/src/main.cpp.o"
+  "CMakeFiles/kat_compiler.dir/src/main.cpp.o.d"
+  "kat_compiler"
+  "kat_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kat_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
